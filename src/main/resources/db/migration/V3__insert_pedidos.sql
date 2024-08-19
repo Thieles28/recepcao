3670fd49c0ff4f3c70574982fad85ed349703b02
Thieles <thielesmartinsdosreis@gmail.com>
2024-08-19 15:23:13 -0300

    Thieles
    Adicionando o script de inserção na base de dados

---
+++ src/main/resources/db/migration/V3__insert_pedidos.sql
@@ -0,0 +1,14 @@
+INSERT INTO pedido (numero_controle, data_cadastro, nome, valor, quantidade, valor_total, codigo_cliente)
+VALUES ('C7890123', '2024-09-22', 'Produto A', 149.99, 6, 899.94, 1);
+
+INSERT INTO pedido (numero_controle, data_cadastro, nome, valor, quantidade, valor_total, codigo_cliente)
+VALUES ('C7890124', '2024-10-01', 'Produto B', 199.99, 2, 399.98, 2);
+
+INSERT INTO pedido (numero_controle, data_cadastro, nome, valor, quantidade, valor_total, codigo_cliente)
+VALUES ('C7890125', '2024-11-10', 'Produto C', 299.99, 10, 2699.91, 3);
+
+INSERT INTO pedido (numero_controle, data_cadastro, nome, valor, quantidade, valor_total, codigo_cliente)
+VALUES ('C7890126', '2024-12-05', 'Produto D', 399.99, 1, 399.99, 4);
+
+INSERT INTO pedido (numero_controle, data_cadastro, nome, valor, quantidade, valor_total, codigo_cliente)
+VALUES ('C7890127', '2024-08-19', 'Produto E', 499.99, 8, 3799.92, 5);
